484ac84fe80ca9f65c3aac64e541541201544382
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-10-02 10:28:12 -0500

    Fix class_name argument

--- app/models/spree/subscription_seat.rb
+++ app/models/spree/subscription_seat.rb
@@ -1,6 +1,6 @@
 class Spree::SubscriptionSeat < ActiveRecord::Base
   belongs_to :account_subscription, class_name: 'Spree::AccountSubscription'
-  belongs_to :user, class_name: Spree.user_class
+  belongs_to :user, class_name: Spree.user_class.to_s
 
   validates :user, uniqueness: { scope: :account_subscription,
                                  message: 'only one redemption per user and subscription' }
