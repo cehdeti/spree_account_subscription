7034866973300b49e810a49f8bc7c5971da9d3bd
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-12-06 17:56:03 -0600

    Fix formatting

--- app/controllers/spree/admin/account_subscriptions_controller.rb
+++ app/controllers/spree/admin/account_subscriptions_controller.rb
@@ -1,16 +1,17 @@
 module Spree
   module Admin
     class AccountSubscriptionsController < ResourceController
-      before_action :load_data, except: :index
 
-      def show
-        redirect_to( action: :edit )
-      end
+      before_action :load_data, except: :index
 
       def index
         params[:q] ||= {}
         @search = Spree::AccountSubscription.search(params[:q])
         @account_subscriptions = @search.result.page(params[:page]).per(15)
+      end
+
+      def show
+        redirect_to(action: :edit)
       end
 
       def create
@@ -24,19 +25,17 @@ module Spree
       protected
 
       def load_data
-        @products = Product.subscribable.all.map { |product| [product.name, product.id] }
-        @users = Spree::User.all.map { |user| [user.email, user.id]}
-
+        @products = Product.subscribable.all.map{|product| [product.name, product.id] }
+        @users = Spree::User.all.map{|user| [ user.email, user.id ] }
       end
 
-      private
+    private
 
       def create_or_update(flash_msg)
         if @account_subscription.update_attributes(subscription_params)
-
           user = Spree::User.find_by(id: subscription_params[:user_id])
-          @account_subscription.user_id=user.id
-          @account_subscription.email=user.email
+          @account_subscription.user_id = user.id
+          @account_subscription.email = user.email
           @account_subscription.save
           redirect_to edit_admin_account_subscription_path(@account_subscription)
           flash.notice = flash_msg
@@ -46,10 +45,12 @@ module Spree
       end
 
       def subscription_params
-        params.require(:account_subscription).permit(:email, :user_id, :subscription_seats,
-                                                     :product_id, :start_datetime, :is_renewal,
-                                                     :end_datetime, :order, :num_seats,
-                                                      :renewing_subscription_id)
+        params.require(:account_subscription).permit(
+          :email, :user_id, :subscription_seats,
+          :product_id, :start_datetime, :is_renewal,
+          :end_datetime, :order, :num_seats,
+          :renewing_subscription_id
+        )
       end
     end
   end
