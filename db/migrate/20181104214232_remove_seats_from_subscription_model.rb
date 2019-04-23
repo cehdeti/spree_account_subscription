83abcc946559b1ec47c6c0e3bb03b45c6f4658fd
Jake Bell <sigterm01@gmail.com>
2019-04-23 11:00:13 -0500

    Fixing broken migration

--- db/migrate/20181104214232_remove_seats_from_subscription_model.rb
+++ db/migrate/20181104214232_remove_seats_from_subscription_model.rb
@@ -1,7 +1,13 @@
 class RemoveSeatsFromSubscriptionModel < ActiveRecord::Migration[5.2]
   def change
-    # remove seat reference from line_items
-    remove_column :spree_line_items, :renewal_seat_id
+    reversible do |dir|
+      # remove seat reference from line_items
+      dir.up do
+        next unless column_exists?(:spree_line_items, :renewal_seat_id)
+        remove_column(:spree_line_items, :renewal_seat_id)
+      end
+      dir.down {}
+    end
 
     # drop the seats table
     drop_table :spree_subscription_seats
